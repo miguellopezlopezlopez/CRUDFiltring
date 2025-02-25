0e51d3d7f3b828278e19a121412ac6adc51650de
Miguel Lopez <lopezlopezm201@gmail.com>
2025-02-25 11:21:47 +0100

    crud casi definitivo

--- CRUDFiltring/MainForm.Designer.cs
+++ CRUDFiltring/MainForm.Designer.cs
@@ -30,9 +30,27 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridViewUsuarios = new System.Windows.Forms.DataGridView();
-            this.btnAgregarUsuario = new System.Windows.Forms.Button();
+            this.btnRegistrarUsuario = new System.Windows.Forms.Button();
             this.btnEliminarUsuario = new System.Windows.Forms.Button();
+            this.btnActualizarUsuario = new System.Windows.Forms.Button();
+            this.pictureBoxFoto = new System.Windows.Forms.PictureBox();
+            this.btnCargarFoto = new System.Windows.Forms.Button();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtUbicacion = new System.Windows.Forms.TextBox();
+            this.cmbGenero = new System.Windows.Forms.ComboBox();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.Apellido = new System.Windows.Forms.Label();
+            this.Genero = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pwd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFoto = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsuarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -55,36 +73,200 @@
             this.dataGridViewUsuarios.Size = new System.Drawing.Size(620, 150);
             this.dataGridViewUsuarios.TabIndex = 1;
             // 
-            // btnAgregarUsuario
+            // btnRegistrarUsuario
             // 
-            this.btnAgregarUsuario.Location = new System.Drawing.Point(110, 297);
-            this.btnAgregarUsuario.Name = "btnAgregarUsuario";
-            this.btnAgregarUsuario.Size = new System.Drawing.Size(140, 44);
-            this.btnAgregarUsuario.TabIndex = 2;
-            this.btnAgregarUsuario.Text = "Agregar Usuario";
-            this.btnAgregarUsuario.UseVisualStyleBackColor = true;
+            this.btnRegistrarUsuario.Location = new System.Drawing.Point(110, 241);
+            this.btnRegistrarUsuario.Name = "btnRegistrarUsuario";
+            this.btnRegistrarUsuario.Size = new System.Drawing.Size(140, 44);
+            this.btnRegistrarUsuario.TabIndex = 2;
+            this.btnRegistrarUsuario.Text = "Agregar Usuario";
+            this.btnRegistrarUsuario.UseVisualStyleBackColor = true;
+            this.btnRegistrarUsuario.Click += new System.EventHandler(this.btnRegistrarUsuario_Click);
             // 
             // btnEliminarUsuario
             // 
-            this.btnEliminarUsuario.Location = new System.Drawing.Point(291, 297);
+            this.btnEliminarUsuario.Location = new System.Drawing.Point(110, 309);
             this.btnEliminarUsuario.Name = "btnEliminarUsuario";
             this.btnEliminarUsuario.Size = new System.Drawing.Size(140, 44);
             this.btnEliminarUsuario.TabIndex = 3;
             this.btnEliminarUsuario.Text = "Eliminar Usuario";
             this.btnEliminarUsuario.UseVisualStyleBackColor = true;
             // 
+            // btnActualizarUsuario
+            // 
+            this.btnActualizarUsuario.Location = new System.Drawing.Point(110, 376);
+            this.btnActualizarUsuario.Name = "btnActualizarUsuario";
+            this.btnActualizarUsuario.Size = new System.Drawing.Size(140, 44);
+            this.btnActualizarUsuario.TabIndex = 4;
+            this.btnActualizarUsuario.Text = "Actualizar Usuario";
+            this.btnActualizarUsuario.UseVisualStyleBackColor = true;
+            this.btnActualizarUsuario.Click += new System.EventHandler(this.btnActualizarUsuario_Click);
+            // 
+            // pictureBoxFoto
+            // 
+            this.pictureBoxFoto.Location = new System.Drawing.Point(581, 241);
+            this.pictureBoxFoto.Name = "pictureBoxFoto";
+            this.pictureBoxFoto.Size = new System.Drawing.Size(183, 101);
+            this.pictureBoxFoto.TabIndex = 5;
+            this.pictureBoxFoto.TabStop = false;
+            // 
+            // btnCargarFoto
+            // 
+            this.btnCargarFoto.Location = new System.Drawing.Point(603, 348);
+            this.btnCargarFoto.Name = "btnCargarFoto";
+            this.btnCargarFoto.Size = new System.Drawing.Size(127, 23);
+            this.btnCargarFoto.TabIndex = 6;
+            this.btnCargarFoto.Text = "Cargar Foto";
+            this.btnCargarFoto.UseVisualStyleBackColor = true;
+            this.btnCargarFoto.Click += new System.EventHandler(this.btnCargarFoto_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(358, 278);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 23);
+            this.txtPwd.TabIndex = 7;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(358, 249);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 23);
+            this.txtUser.TabIndex = 8;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(358, 307);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 9;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(358, 336);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 23);
+            this.txtApellido.TabIndex = 10;
+            // 
+            // txtUbicacion
+            // 
+            this.txtUbicacion.Location = new System.Drawing.Point(358, 397);
+            this.txtUbicacion.Name = "txtUbicacion";
+            this.txtUbicacion.Size = new System.Drawing.Size(100, 23);
+            this.txtUbicacion.TabIndex = 11;
+            // 
+            // cmbGenero
+            // 
+            this.cmbGenero.FormattingEnabled = true;
+            this.cmbGenero.Items.AddRange(new object[] {
+            "Masculino",
+            "Femenino",
+            "Prefiero no decirlo"});
+            this.cmbGenero.Location = new System.Drawing.Point(358, 365);
+            this.cmbGenero.Name = "cmbGenero";
+            this.cmbGenero.Size = new System.Drawing.Size(100, 23);
+            this.cmbGenero.TabIndex = 12;
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(286, 252);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(47, 15);
+            this.Usuario.TabIndex = 13;
+            this.Usuario.Text = "Usuario";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(286, 307);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(51, 15);
+            this.Nombre.TabIndex = 14;
+            this.Nombre.Text = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.AutoSize = true;
+            this.Apellido.Location = new System.Drawing.Point(286, 339);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(51, 15);
+            this.Apellido.TabIndex = 15;
+            this.Apellido.Text = "Apellido";
+            // 
+            // Genero
+            // 
+            this.Genero.AutoSize = true;
+            this.Genero.Location = new System.Drawing.Point(286, 373);
+            this.Genero.Name = "Genero";
+            this.Genero.Size = new System.Drawing.Size(45, 15);
+            this.Genero.TabIndex = 16;
+            this.Genero.Text = "Género";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(286, 405);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Ubicación";
+            // 
+            // pwd
+            // 
+            this.pwd.AutoSize = true;
+            this.pwd.Location = new System.Drawing.Point(286, 281);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(67, 15);
+            this.pwd.TabIndex = 18;
+            this.pwd.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 30);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Ruta de la foto \r\n(Se genera al seleccionar la imagen)";
+            // 
+            // txtFoto
+            // 
+            this.txtFoto.Location = new System.Drawing.Point(620, 388);
+            this.txtFoto.Name = "txtFoto";
+            this.txtFoto.Size = new System.Drawing.Size(100, 23);
+            this.txtFoto.TabIndex = 20;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFoto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pwd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Genero);
+            this.Controls.Add(this.Apellido);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.cmbGenero);
+            this.Controls.Add(this.txtUbicacion);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.btnCargarFoto);
+            this.Controls.Add(this.pictureBoxFoto);
+            this.Controls.Add(this.btnActualizarUsuario);
             this.Controls.Add(this.btnEliminarUsuario);
-            this.Controls.Add(this.btnAgregarUsuario);
+            this.Controls.Add(this.btnRegistrarUsuario);
             this.Controls.Add(this.dataGridViewUsuarios);
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Text = "MainForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsuarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,7 +276,24 @@
 
         private Label label1;
         private DataGridView dataGridViewUsuarios;
-        private Button btnAgregarUsuario;
+        private Button btnRegistrarUsuario;
         private Button btnEliminarUsuario;
+        private Button btnActualizarUsuario;
+        private PictureBox pictureBoxFoto;
+        private Button btnCargarFoto;
+        private TextBox txtPwd;
+        private TextBox txtUser;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtUbicacion;
+        private ComboBox cmbGenero;
+        private Label Usuario;
+        private Label Nombre;
+        private Label Apellido;
+        private Label Genero;
+        private Label label6;
+        private Label pwd;
+        private Label label2;
+        private TextBox txtFoto;
     }
 }
--- CRUDFiltring/MainForm.cs
+++ CRUDFiltring/MainForm.cs
@@ -13,6 +13,7 @@ namespace FiltringApp
     {
         private MySqlConnection conexion;
         private string cadenaConexion;
+        private string rutaFoto = string.Empty;
 
         public MainForm()
         {
@@ -56,7 +57,7 @@ namespace FiltringApp
             try
             {
                 conexion.Open();
-                string consulta = "SELECT ID, User, Nombre, Apellido, Genero, Ubicacion FROM Usuario";
+                string consulta = "SELECT *  FROM Usuario";
                 MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
                 DataTable dt = new DataTable();
                 adaptador.Fill(dt);
@@ -92,7 +93,9 @@ namespace FiltringApp
                     cmd.Parameters.AddWithValue("@id", id);
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("Usuario eliminado correctamente.");
-                }
+                    CargarDatosUsuarios();
+                    }
+                    
                 catch (Exception ex)
                 {
                     MessageBox.Show("Error al eliminar usuario: " + ex.Message);
@@ -100,8 +103,107 @@ namespace FiltringApp
                 finally
                 {
                     conexion.Close();
+                }
+            }
+        }
+
+        private void btnActualizarUsuario_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewUsuarios.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dataGridViewUsuarios.SelectedRows[0].Cells["ID"].Value);
+                string usuario = txtUser.Text;
+                string contraseña = txtPwd.Text;
+                string nombre = txtNombre.Text;
+                string apellido = txtApellido.Text;
+                string genero = cmbGenero.SelectedItem?.ToString() ?? "";
+                string ubicacion = txtUbicacion.Text;
+
+                if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contraseña) || string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido) || string.IsNullOrEmpty(genero) || string.IsNullOrEmpty(rutaFoto) || string.IsNullOrEmpty(ubicacion))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                try
+                {
+                    conexion.Open();
+                    string consulta = "UPDATE Usuario SET User=@user, Password=@password, Nombre=@nombre, Apellido=@apellido, Genero=@genero, Foto=@foto, Ubicacion=@ubicacion WHERE ID=@id";
+                    MySqlCommand cmd = new MySqlCommand(consulta, conexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@user", usuario);
+                    cmd.Parameters.AddWithValue("@password", contraseña);
+                    cmd.Parameters.AddWithValue("@nombre", nombre);
+                    cmd.Parameters.AddWithValue("@apellido", apellido);
+                    cmd.Parameters.AddWithValue("@genero", genero);
+                    cmd.Parameters.AddWithValue("@foto", rutaFoto);
+                    cmd.Parameters.AddWithValue("@ubicacion", ubicacion);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Usuario actualizado correctamente.");
                     CargarDatosUsuarios();
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al actualizar usuario: " + ex.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+            }
+        }
+
+        private void btnCargarFoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                rutaFoto = openFileDialog1.FileName;
+                pictureBoxFoto.Image = Image.FromFile(rutaFoto);
+                txtFoto.Text = rutaFoto;
+            }
+        }
+
+        private void btnRegistrarUsuario_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string contraseña = txtPwd.Text;
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string genero = cmbGenero.SelectedItem?.ToString() ?? "";
+            string ubicacion = txtUbicacion.Text;
+
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contraseña) || string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(apellido) || string.IsNullOrEmpty(genero) || string.IsNullOrEmpty(rutaFoto) || string.IsNullOrEmpty(ubicacion))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                conexion.Open();
+                string consulta = "INSERT INTO Usuario (User, Password, Nombre, Apellido, Genero, Foto, Ubicacion) VALUES (@user, @password, @nombre, @apellido, @genero, @foto, @ubicacion)";
+                MySqlCommand cmd = new MySqlCommand(consulta, conexion);
+                cmd.Parameters.AddWithValue("@user", usuario);
+                cmd.Parameters.AddWithValue("@password", contraseña);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@apellido", apellido);
+                cmd.Parameters.AddWithValue("@genero", genero);
+                cmd.Parameters.AddWithValue("@foto", rutaFoto);
+                cmd.Parameters.AddWithValue("@ubicacion", ubicacion);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registro completado exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CargarDatosUsuarios();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar usuario: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.Close();
             }
         }
     }
